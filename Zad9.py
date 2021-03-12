cd7801c1ee7ecbf6310eadfb2743c0b4e9f66fa3
selfwezmir <self.wezmir@gmail.com>
2021-03-12 17:45:21 +0100

    Update Zad9.py

--- Zad9.py
+++ Zad9.py
@@ -1,8 +1,10 @@
 from ciagi import *
 
-An = arytmetyczne.n_wyraz(1, 3, 76)
+An = arytmetyczne.n_wyraz(1, 3, 35)
 print(An)
-print(arytmetyczne.n_suma(1, An, 76))
+print(arytmetyczne.n_suma(1, An, 35))
+print(arytmetyczne.n_iloczyn(1, 3, 35))
 
 print(geometryczne.n_wyraz(1, 6, 7))
 print(geometryczne.n_suma(1, 6, 7))
+print(geometryczne.n_iloczyn(1, 6, 7))

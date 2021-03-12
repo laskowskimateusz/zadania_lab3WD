403acf6d08bbebdf1a214796a2873ea5d677b8a3
selfwezmir <self.wezmir@gmail.com>
2021-03-12 17:03:14 +0100

    Update Zad6.py

--- Zad6.py
+++ Zad6.py
@@ -1,5 +1,15 @@
-def iloczyn_ciagu(a=1, b=4, ile=10):
+def suma_ciagu(a=1, b=4, ile=10):
     return a * ((1 - (b**ile))/(1-b))
 
 
-print(iloczyn_ciagu())
+def iloczyn_ciagu(a=1, b=4, ile=10):
+    ciag = [a]
+    suma = 1
+    for x in range(a, ile):
+        ciag.append(ciag[-1] * b)
+        suma *= ciag[-1]
+    return suma
+
+
+print("Suma elementów ciągu: ", int(suma_ciagu()))
+print("Iloczyn elementów ciągu: ", iloczyn_ciagu())

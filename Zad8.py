2539c68455dc2c10b0475154ea3311b47ea4341c
selfwezmir <self.wezmir@gmail.com>
2021-03-12 17:20:26 +0100

    Update Zad8.py

--- Zad8.py
+++ Zad8.py
@@ -1,5 +1,5 @@
 def zlicz(**produkty):
-    return ("Ilość produktów", len(produkty.keys())), ("Całościowa wartość produktów", sum(produkty.values()))
+    return {"Ilość produktów": len(produkty.keys()), "Całościowa wartość produktów": sum(produkty.values())}
 
 
 print(zlicz(pomidor=5, papryka=4, mąka=3, mleko=3, woda=2, chleb=3))

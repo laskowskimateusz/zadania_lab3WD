cd1ae8ed7fe9c27c5121f0a7a2447b9d1a1dda33
selfwezmir <self.wezmir@gmail.com>
2021-03-12 17:45:12 +0100

    Update geometryczne.py

--- ciagi/geometryczne.py
+++ ciagi/geometryczne.py
@@ -4,3 +4,12 @@ def n_wyraz(a1, q, n):
 
 def n_suma(a1, q, n):
     return a1 * ((1 - (q**n))/(1-q))
+
+
+def n_iloczyn(a1, q, n):
+    ciag = [a1]
+    suma = 1
+    for x in range(a1, n):
+        ciag.append(ciag[-1] * q)
+        suma *= ciag[-1]
+    return suma

bb9279791f9cd35916dbe1da4fbb2f488d5d2ed4
selfwezmir <self.wezmir@gmail.com>
2021-03-12 17:03:51 +0100

    Update Zad7.py

--- Zad7.py
+++ Zad7.py
@@ -1,7 +1,18 @@
-def iloczyn_ciagu(*liczby):
+def suma_ciagu(*liczby):
     q = liczby[1]/liczby[0]
     n = len(liczby)
     return liczby[0] * ((1 - (q**n))/(1-q))
 
 
-print(iloczyn_ciagu(1, 5, 25, 125, 625, 3125))
+def iloczyn_ciagu(*liczby):
+    ciag = [liczby[0]]
+    q = liczby[1]/liczby[0]
+    suma = 1
+    for x in range(len(liczby)):
+        ciag.append(ciag[-1] * q)
+        suma *= ciag[-1]
+    return suma
+
+
+print("Suma elementów ciągu: ", suma_ciagu(1, 5, 25, 125, 625, 3125))
+print("Iloczyn elementów ciągu: ", iloczyn_ciagu(1, 5, 25, 125, 625, 3125))

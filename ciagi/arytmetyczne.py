68b791b61e1fe32dba881c156614897f6df97c0a
selfwezmir <self.wezmir@gmail.com>
2021-03-12 17:45:03 +0100

    Update arytmetyczne.py

--- ciagi/arytmetyczne.py
+++ ciagi/arytmetyczne.py
@@ -4,3 +4,12 @@ def n_wyraz(a1, r, n):
 
 def n_suma(a1, An, n):
     return ((a1 + An)/2)*n
+
+
+def n_iloczyn(a1, r, n):
+    ciag = [a1]
+    suma = 1
+    for x in range(a1, n):
+        ciag.append(ciag[-1] + r)
+        suma *= ciag[-1]
+    return suma
